7abf53154b8c92bdb5a5f34384bcea325fc3991e
Morten Falk <mfjo@itu.dk>
2022-10-28 13:33:14 +0200

    pathing structure for directing to repository

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using LibGit2Sharp;
+
+string workingDirectory = Environment.CurrentDirectory;
+string newPath = Directory.GetParent(workingDirectory).Parent.Parent.Parent.FullName;
+using (var repo = new Repository(newPath))
+    
+    ///Users/morten/Documents/GitHub/BDSA-projekt/GitInsight/Program.cs
+{
+    
+    var headCommit = repo.Head.Commits.First();
+    Console.WriteLine(headCommit);
+    
+}
+
