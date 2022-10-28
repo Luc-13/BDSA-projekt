7a8f0ed9ab6c52c2312033006307b60c5c710a2c
lufr@itu.dk <lufr@itu.dk>
2022-10-28 12:44:58 +0200

    outline for tests

--- GitInsight.Tests/UnitTest1.cs
+++ GitInsight.Tests/UnitTest1.cs
@@ -3,8 +3,26 @@ namespace GitInsight.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Check_overall_returns_correct_int()
     {
+        throw new NotImplementedException();
+    }
 
+    [Fact]
+    public void Check_users_returns_correct_users_and_ints()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void Wrong_path_returns_no_path_exception()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void Path_without_repo_returns_no_repo_exception()
+    {
+        throw new NotImplementedException();
     }
 }
