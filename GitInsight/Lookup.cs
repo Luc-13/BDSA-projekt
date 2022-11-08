59430ffbeaed92d8956dd090a0773adabc585956
lufr@itu.dk <lufr@itu.dk>
2022-11-08 14:29:49 +0100

    setup core methods to clone/fetch repos from URLs

--- GitInsight/Lookup.cs
+++ GitInsight/Lookup.cs
@@ -1,17 +1,18 @@
 namespace GitInsight;
 using GitInsight;
 using LibGit2Sharp;
+using System;
 
 public class Lookup
 {
-    
+
     public void authorMode(string repopath)
     {
         List<User> userlist = new List<User>();
         var map = new Dictionary<string, User>();
         using (var repo = new Repository(repopath))
 
-            ///Users/morten/Documents/GitHub/BDSA-projekt/GitInsight/Program.cs
+        ///Users/morten/Documents/GitHub/BDSA-projekt/GitInsight/Program.cs
         {
 
             var headCommit = repo.Head.Commits.ToList();
@@ -36,15 +37,15 @@ public class Lookup
             {
                 Console.WriteLine(user.userName);
                 var histogram = from c in user.commitlist
-                    group c by c.date
+                                group c by c.date
                     into h
-                    let i = h.Count()
-                    orderby h.Key
-                    select new
-                    {
-                        Date = h.Key,
-                        Count = i
-                    };
+                                let i = h.Count()
+                                orderby h.Key
+                                select new
+                                {
+                                    Date = h.Key,
+                                    Count = i
+                                };
 
                 foreach (var commit in histogram)
                 {
@@ -65,16 +66,16 @@ public class Lookup
             var headCommit = repo.Head.Commits.ToList();
 
             var histogram = from c in headCommit
-                group c by c.Author.When.Date
+                            group c by c.Author.When.Date
                 into h
-                let i = h.Count()
-                orderby h.Key
-                select new
-                {
-                    Date = h.Key,
-                    Count = i
-                };
-            
+                            let i = h.Count()
+                            orderby h.Key
+                            select new
+                            {
+                                Date = h.Key,
+                                Count = i
+                            };
+
             foreach (var commit in histogram)
             {
                 //Console.WriteLine(commit.Count + " " + commit.Date.ToString("yyyy-MM-dd"));
@@ -82,9 +83,42 @@ public class Lookup
             }
 
             Console.WriteLine("");
-            
         }
-
-        
     }
+    public void Clone(string temppath, string repopath)
+    {
+        var url = ("https://www.github.com/" + repopath);
+        Repository.Clone(url, temppath);
+        Console.WriteLine(temppath);
+    }
+    public void FetchPull(string repopath)
+    {
+        using (var repo = new Repository(repopath))
+        {
+            var options = new FetchOptions();
+            options.Prune = true;
+            options.TagFetchMode = TagFetchMode.Auto;
+            var remote = repo.Network.Remotes["origin"];
+            var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
+            Commands.Fetch(repo, remote.Name, refSpecs, options, "fetching");
+
+            PullOptions pullOptions = new PullOptions();
+            pullOptions.MergeOptions = new MergeOptions();
+            pullOptions.MergeOptions.FailOnConflict = true;
+            Commands.Pull(repo, new Signature(name: "main", email: "mail", when: DateTimeOffset.Now), pullOptions);
+        }
+        Console.WriteLine("fetchpull done");
+    }
+    public void CheckRepo(string repopath, string repo){
+        Console.WriteLine("Checking repo");
+        var temppath = System.IO.Path.GetTempPath();
+        string[] subdirs = Directory.GetDirectories(temppath);
+
+        if(!subdirs.Contains(repo)){
+            Clone(temppath, repopath);
+        }else{
+            FetchPull(temppath + "/" + repopath);
+        }
+    }
+
 }
